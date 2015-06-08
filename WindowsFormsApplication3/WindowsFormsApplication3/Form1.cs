1ac234a575e3155af4703b8ac464e4dc01898d53
Mario Vilaj <vilaj.mario@gmail.com>
2015-06-08 23:37:39 +0200

    setup

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwind40DataSet.Orders' table. You can move, or remove it, as needed.
+            this.ordersTableAdapter.Fill(this.northwind40DataSet.Orders);
+
+        }
+
+        private void ordersDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            webBrowser1.Navigate("http://maps.google.com/maps?q=" + ordersDataGridView.SelectedCells[0].Value.ToString());
+        }
+    }
+}
